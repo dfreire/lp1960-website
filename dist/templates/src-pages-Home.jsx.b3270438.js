cab528fd040e41769539786eb54e39ba351463f8
dfreire <dario.freire@gmail.com>
2018-08-22 19:13:53 +0100

    added dist to the repo

---
+++ dist/templates/src-pages-Home.jsx.b3270438.js
@@ -0,0 +1,1 @@
+webpackJsonp([3],{53:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=n(1),r=function(e){return e&&e.__esModule?e:{default:e}}(u),o=n(27);t.default=function(e){return r.default.createElement(o.Redirect,{to:"/intro"})}}});
