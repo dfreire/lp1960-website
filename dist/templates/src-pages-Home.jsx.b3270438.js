8ee4082054dcf1fbca707e5ed48cf9065b51cb11
dfreire <dario.freire@gmail.com>
2018-08-22 19:37:59 +0100

    removed dist/ from the repo

--- dist/templates/src-pages-Home.jsx.b3270438.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([3],{53:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=n(1),r=function(e){return e&&e.__esModule?e:{default:e}}(u),o=n(27);t.default=function(e){return r.default.createElement(o.Redirect,{to:"/intro"})}}});
